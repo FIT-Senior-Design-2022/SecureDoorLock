60c9b7e2e2245ae1cce26786aaad7ce37caa8890
LukeBucher <lukebucherwork@gmail.com>
2022-09-02 12:23:06 -0400

    Initial PDF component

--- project-page/src/App.js
+++ project-page/src/App.js
@@ -1,8 +1,41 @@
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+
+
 
 function App() {
   return (
-    <a href="assets/SeniorProjectPlan.pdf" target="_blank">Project Plan</a>
+    <div id="readerGroup">
+      <Card
+        sx={{
+          minHeight: 300,
+          display: "inline-flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          boxShadow: 5,
+        }}
+      >
+        <CardContent sx={{
+          
+        }}>
+          <div>
+            <ButtonGroup>
+              <Button>Project Plan</Button>
+              <Button disabled>Milestone 1</Button>
+              <Button disabled>Milestone 2</Button>
+              <Button disabled>Milestone 3</Button>
+            </ButtonGroup>
+          </div>
+          <div>
+              <a href="./assets/SeniorProjectPlan.pdf">Project Plan</a>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
