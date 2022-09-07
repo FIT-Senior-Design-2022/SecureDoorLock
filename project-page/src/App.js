72191f389c6d5f96bbea22f37c3d99262341bc71
LuekBucher <lukebucherwork@gmail.com>
2022-09-07 16:11:40 -0400

    Fix error

--- project-page/src/App.js
+++ project-page/src/App.js
@@ -4,24 +4,6 @@ import "./App.css";
 import ProjectPlan from "./Tabs/ProjectPlan";
 import { Box } from "@mui/system";
 import { AppBar, Button, Tab, Tabs, Toolbar, Card, CardContent } from "@mui/material";
-import { createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  status: {
-    danger: '#e53e3e',
-  },
-  palette: {
-    primary: {
-      main: '#0971f1',
-      darker: '#053e85',
-    },
-    neutral: {
-      main: '#64748B',
-      contrastText: '#fff',
-    },
-  },
-});
-
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
