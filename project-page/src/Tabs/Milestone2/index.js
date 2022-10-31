8be6df50ef264fc16baf76e9e251b939d9f1e114
LuekBucher <lukebucherwork@gmail.com>
2022-10-31 16:22:00 -0400

    Fix

--- project-page/src/Tabs/Milestone2/index.js
+++ project-page/src/Tabs/Milestone2/index.js
@@ -5,7 +5,7 @@ export const MileStone2 = () => {
   return (
     <DocumentLayout documentlist={[
         {name:"Progress Evaluation",target:"SecureDoorOpenerMilestone2.pdf"},
-        {name:"Milestone Presentation",target:"Design-SecureDoorLock.pdf"},
+        {name:"Milestone Presentation",target:"Presentation2.pdf"},
     ]}/>
   );
 }
