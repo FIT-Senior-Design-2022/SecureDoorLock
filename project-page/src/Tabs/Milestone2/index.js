f2376640c5670eeae56055b970e956ce7f0070fb
LuekBucher <lukebucherwork@gmail.com>
2022-10-31 14:18:11 -0400

    Added needed resources for Milestone 2

---
+++ project-page/src/Tabs/Milestone2/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DocumentLayout from "../../Components/DocumentLayout";
+
+export const MileStone2 = () => {
+  return (
+    <DocumentLayout documentlist={[
+        {name:"Progress Evaluation",target:"SecureDoorOpenerMilestone2.pdf"},
+        {name:"Milestone 2 Presentation",target:"Design-SecureDoorLock.pdf"},
+    ]}/>
+  );
+}
+
+export default MileStone2;
